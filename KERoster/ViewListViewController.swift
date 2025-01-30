cdcef738ef6ca06ff49f5de8a2bbdee42b636933
Jungsub Yoon <cage@live.co.kr>
2025-01-30 15:40:06 +0900

    스케줄 확인 추가 페이지 구현

---
+++ KERoster/ViewListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewListViewController.swift
+//  KERoster
+//
+//  Created by 윤정섭 on 2025/01/30.
+//
+
+import UIKit
+
+class ViewListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
