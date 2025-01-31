fc314f59834a472a19a237b8b3b96b391d531c2b
Jungsub Yoon <cage@live.co.kr>
2025-01-31 13:35:57 +0900

    ScheduleDetailViewController 구현

---
+++ KERoster/ScheduleDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScheduleDetailViewController.swift
+//  KERoster
+//
+//  Created by 윤정섭 on 2025/01/31.
+//
+
+import UIKit
+
+class ScheduleDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
