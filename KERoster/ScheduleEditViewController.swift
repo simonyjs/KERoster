58d08694cb8b88dcc0f3fd9c96312af7a18e8005
Jungsub Yoon <cage@live.co.kr>
2025-02-02 22:37:05 +0900

    ScheduleEditViewController 추가 및 기타 UI 개선

---
+++ KERoster/ScheduleEditViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScheduleEditViewController.swift
+//  KERoster
+//
+//  Created by 윤정섭 on 2025/02/02.
+//
+
+import UIKit
+
+class ScheduleEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
