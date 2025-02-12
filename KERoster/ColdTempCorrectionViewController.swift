45b1e80b57293173da7ab9af2e873c2acdb267c0
Jungsub Yoon <cage@live.co.kr>
2025-02-12 18:48:25 +0900

    V 2.0 Tools 구현

---
+++ KERoster/ColdTempCorrectionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ColdTempCorrectionViewController.swift
+//  KERoster
+//
+//  Created by 윤정섭 on 2025/02/12.
+//
+
+import UIKit
+
+class ColdTempCorrectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ KERoster/ToolsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ToolsViewController.swift
+//  KERoster
+//
+//  Created by 윤정섭 on 2025/02/12.
+//
+
+import UIKit
+
+class ToolsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
