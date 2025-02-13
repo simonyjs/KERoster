3cf8a4047c6b70b2a45bd8115d17a94a882ee074
Jungsub Yoon <cage@live.co.kr>
2025-02-13 20:19:27 +0900

    스케줄 입력을 달력으로 보면서 입력으로 구현

---
+++ KERoster/ScheduleInputFormViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScheduleInputFormViewController.swift
+//  KERoster
+//
+//  Created by 윤정섭 on 2025/02/13.
+//
+
+import UIKit
+
+class ScheduleInputFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
