227745eaf4ed628c155a5624c5890706e229d747
Jungsub Yoon <cage@live.co.kr>
2025-02-16 00:31:40 +0900

    지역에 관계없이 휴일정보를 한국 기준으로 출력 완료 및 공항별 UTC 작업 초기 단계

---
+++ KERoster/Airport.swift
@@ -0,0 +1,29 @@
+//
+//  Airport.swift
+//  KERoster
+//
+//  Created by 윤정섭 on 2025/02/15.
+//
+
+import UIKit
+
+class Airport: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
