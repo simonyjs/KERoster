2a3cb1bf58d43d4fcbeab5fcee73f9efc9bc5084
Jungsub Yoon <cage@live.co.kr>
2025-02-22 21:19:49 +0900

    스케줄 수동 입력 문제 개선(별도의 키를 배당하여 웹에서 스케줄을 가져오더라도 수동으로 입력한 스케줄은 지워지지 않음, 캘린더 내보내기 안되는 오류 개선, 기본 타임존은 GMP로 성정하고 사용자가 선택 가능하게 함)

---
+++ KERoster/ScheduleInputFormModifiedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScheduleInputFormModifiedViewController.swift
+//  KERoster
+//
+//  Created by 윤정섭 on 2025/02/22.
+//
+
+import UIKit
+
+class ScheduleInputFormModifiedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
