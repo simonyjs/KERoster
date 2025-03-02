da86aca313d47278b391e2371bfa9896bde440da
Jungsub Yoon <cage@live.co.kr>
2025-03-02 13:12:16 +0900

    위젯 추가 초기 버전

---
+++ KERosterWidget/NextFlightWidget.swift
@@ -0,0 +1,8 @@
+//
+//  NextFlightWidget.swift
+//  KERoster
+//
+//  Created by 윤정섭 on 2025/03/02.
+//
+
+import Foundation
---
+++ KERosterWidget/containerBackground.swift
@@ -0,0 +1,8 @@
+//
+//  containerBackground.swift
+//  KERoster
+//
+//  Created by 윤정섭 on 2025/03/02.
+//
+
+import Foundation
